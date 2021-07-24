989375f47c97ded1591ff6bab8323fdaecb5a42f
panca29 <74854420+panca29@users.noreply.github.com>
2021-07-24 15:47:48 +0700

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-printf "Installing RDP Be Patience... " >&2
+printf "setup loading" >&2
 {
-sudo useradd -m enigma
-sudo adduser enigma sudo
-echo 'strongpw' | sudo chpasswd
+sudo useradd -m ALOK
+sudo adduser ALOK sudo
+echo 'ALOK:8426' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -18,17 +18,16 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser enigma chrome-remote-desktop
+sudo apt -y install obs-studio
+sudo apt -y install firefox
+sudo apt -y install qbittorrent
+sudo apt install nload
+sudo adduser ALOK chrome-remote-desktop
 } &> /dev/null &&
-printf "\nSetup Complete " >&2 ||
+printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
-read -p "Paste Here:& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AX4XfWgYC-yCtInVd7kZ-AoA623kioK4-z7vJZk6QJizhje9NXJKYWXT3h9DoB64P8fs1Q" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME
-printf 'Check https://remotedesktop.google.com/access/ \n\n'
-if sudo apt-get upgrade &> /dev/null
-then
-    printf "\n\nUpgrade Completed " >&2
-else
-    printf "\n\nError Occured " >&2
-fi
-
+read -p "& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AX4XfWgYC-yCtInVd7kZ-AoA623kioK4-z7vJZk6QJizhje9NXJKYWXT3h9DoB64P8fs1Q" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME" CRP
+su - ALOK -c """$CRP"""
+printf 'Check https://remotedesktop.google.com/access/ \n'
+printf 'Your SUDO Pasword Is 8426 \n'
